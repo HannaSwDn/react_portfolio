fa11c7afa19da0959160525abb0648055735cde9
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-02 21:35:57 +0100

    Creates Hello.jsx file, adding class Hello to Hello.jsx, renames Helloworld function in index.js to App

---
+++ src/Hello.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "React"
+
+class Hello extends Component {
+    render() {
+        return(
+            <div>Hello World</div>
+        )
+    }
+}
+
+export default Hello
