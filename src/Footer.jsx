9d2c67450a9ba972b6727d169b017a307e66f932
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-03 21:43:20 +0100

    Creates Footer class in Footer.jsx and exporting it

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <h1>Made with React</h1>
+            </footer>
+        )
+    }
+}
+
+export default Footer
