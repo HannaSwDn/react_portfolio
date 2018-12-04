3982bafa3e52810c0dbe6dd86eed9941702c4488
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-04 21:54:42 +0100

    Imports components from About.jsx and Projects.jsx, imports BrowserRouter(?)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom"
 import Hello from "./Hello"
 import Header from "./Header"
 import Footer from "./Footer"
+import { BrowserRouter } from 'react-router-dom'
+import Projects from "./Projects"
+import About from "./About"
 import "./css/tailwind.css"
 
 const App = () => {
