ccf0d032f04e16d7b70b11c11146c87118a10ac5
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-05 22:22:43 +0100

    Imports Axios, sets projects inside the constructor to an empty array

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,20 +1,11 @@
 import React, { Component } from "react"
+import axios from "axios"
 
 class Projects extends Component {
     constructor() {
         super();
         this.state = {
-            projects: [
-                        {
-                            "id": 1,
-                            "name": "My First Website"
-                        },
-                        {
-                            "id": 2,
-                            "name": "FizzBuzz"
-                        }
-
-                    ]
+            projects: []
         };
     }
 
