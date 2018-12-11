00362da0537f433d93cacfbcfbb267ec61df9620
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-11 21:08:11 +0100

    Adds 'Working' illustration from unDraw to about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,9 +1,23 @@
 import React from "react"
+import { UndrawWorking } from 'react-undraw-illustrations';
 
 const About = () => (
     <div>
         <h1>About Me</h1>
         <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+
+        <div className="content-wrapper">
+            <div className="flex flex-wrap mb-4">
+                <div className="w-full lg:w-1/4 md:w-1/2">
+                    <UndrawWorking />
+                </div>
+                <div className="w-full lg:w-3/4 md:w-1/2">
+                    <h1 className="content-text">About Me</h1>
+                    <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+                </div>
+
+            </div>
+        </div>
     </div>
 );
 
